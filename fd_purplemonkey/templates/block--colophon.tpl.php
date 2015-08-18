7e1866ebbd2603be1fd8b95752c9ebee3568b7d1
Kyle GrGreyvenstein <kyle@cognasium.com>
2015-08-18 11:58:29 +0200

    Kyle: removing theme

--- fd_purplemonkey/templates/block--colophon.tpl.php
+++
@@ -1,6 +0,0 @@
-<section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php // blocks in this region does not render a title ?>
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php print $content ?>
-  </div>
-</section> <!-- /.block -->
